fd971baa527be9112562ef4214d0babd92d71a75
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-10 22:05:00 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 def index(request):
-    return render(request, 'index.html')
+    moscow_legends = staticfiles_storage.url("places/moscow_legends.json")
+    roofs_24 = staticfiles_storage.url("places/roofs24.json")
+
+    context = {"value": {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": moscow_legends,
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": roofs_24,
+          }
+        }
+      ]
+    }}
+    return render(request, 'index.html', context=context)
