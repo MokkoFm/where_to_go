bab39d3a7fb2773d23a37e735c912293fa624c8d
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-10 21:05:52 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -123,5 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "static", "places")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,4 +1,8 @@
 from places import views
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 """where_to_go URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,10 +18,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
