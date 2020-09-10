2d76729ed3c93ff4f70004b6b501dce14fbc0aed
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-10 20:53:47 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -3,10 +3,18 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
-    description_short = models.CharField(max_length=400)
-    description_long = models.TextField()
+    description_short = models.CharField(max_length=400, null=True, blank=True)
+    description_long = models.TextField(null=True, blank=True)
     lng = models.DecimalField(max_digits=19, decimal_places=16)
     lat = models.DecimalField(max_digits=19, decimal_places=16)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    picture = models.ImageField(upload_to="places", null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,3 +124,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static", "places")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
