61baa07b0d86598a2fa101df9210e96675ff7dfc
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-11 18:13:04 +0300

    get geojson points from database

--- places/models.py
+++ places/models.py
@@ -5,8 +5,8 @@ class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=400, null=True, blank=True)
     description_long = models.TextField(null=True, blank=True)
-    lng = models.DecimalField(max_digits=19, decimal_places=16)
-    lat = models.DecimalField(max_digits=19, decimal_places=16)
+    lng = models.DecimalField(max_digits=17, decimal_places=14)
+    lat = models.DecimalField(max_digits=17, decimal_places=14)
 
     def __str__(self):
         return f"{self.title}"
--- places/views.py
+++ places/views.py
@@ -1,38 +1,35 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.storage import staticfiles_storage
+from .models import Place
 
 
 def index(request):
     moscow_legends = staticfiles_storage.url("places/moscow_legends.json")
-    roofs_24 = staticfiles_storage.url("places/roofs24.json")
+    #roofs_24 = staticfiles_storage.url("places/roofs24.json")
+    places = Place.objects.all()
+    places_info = []
 
-    context = {"value": {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": moscow_legends,
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": roofs_24,
-          }
+    for place in places:
+        info = {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+              },
+              "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": moscow_legends,
+              }
+            },
+          ]
         }
-      ]
-    }}
+
+        places_info.append(info)
+
+    context = {"value": places_info}
+
     return render(request, 'index.html', context=context)
