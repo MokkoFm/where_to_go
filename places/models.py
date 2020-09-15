34eadf793ed7a2451b6d0956d572e1e7e501b3f9
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-15 10:56:32 +0300

    add WYSIWYG for long description

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=400, null=True, blank=True)
-    description_long = models.TextField(null=True, blank=True)
+    description_long = HTMLField(null=True, blank=True)
     lng = models.DecimalField(max_digits=17, decimal_places=14)
     lat = models.DecimalField(max_digits=17, decimal_places=14)
 
@@ -17,7 +18,7 @@ class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE,
                               related_name="images", default=None,
                               null=True, blank=True)
-    position = models.PositiveIntegerField(null=True, blank=True)
+    position = models.PositiveIntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return f"{self.position} {self.place}"
