a0ef95419d19b1f3cdb60f3a580504cc899f10bf
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-15 12:52:56 +0300

    add readme, add verbose_name for fields, change decimal to float fields

--- places/models.py
+++ places/models.py
@@ -3,22 +3,27 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.CharField(max_length=400, null=True, blank=True)
-    description_long = HTMLField(null=True, blank=True)
-    lng = models.DecimalField(max_digits=17, decimal_places=14)
-    lat = models.DecimalField(max_digits=17, decimal_places=14)
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description_short = models.CharField(
+        max_length=400, null=True, blank=True,
+        verbose_name="Short description")
+    description_long = HTMLField(
+        null=True, blank=True, verbose_name="Long description")
+    lng = models.FloatField(blank=True, verbose_name="Longitude")
+    lat = models.FloatField(blank=True, verbose_name="Latitude")
 
     def __str__(self):
         return f"{self.title}"
 
 
 class Image(models.Model):
-    picture = models.ImageField(upload_to="places", null=True, blank=True)
+    picture = models.ImageField(
+        upload_to="places", null=True, blank=True, verbose_name="Picture")
     place = models.ForeignKey(Place, on_delete=models.CASCADE,
-                              related_name="images", default=None,
-                              null=True, blank=True)
-    position = models.PositiveIntegerField(null=True, blank=True, default=0)
+                              related_name="images", default=None, null=True,
+                              blank=True, verbose_name="Place of picture")
+    position = models.PositiveIntegerField(
+        null=True, blank=True, default=0, verbose_name="Number of picture")
 
     def __str__(self):
         return f"{self.position} {self.place}"
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Where to go - interesting places in Moscow
+
+You can find and get more information about amazing places in Moscow.
+
+## Installation
+
+Python 3 must be installed. Download the code.
+
+### Environment Variables 
+
+Some of settings are taken from environment variables. Create `.env` near `manage.py` and write data there in this format:`VARIABLE=value`.
+
+There're 3 variables:
+- `SECRET-KEY` - project's secret key.
+- `DEBUG` - (default:`False`) debug mode. Set `True` if you need to debug the project.
+- `DATABASE` - project's name of database.
+
+### Requirements
+
+Install requirements with command:
+```
+pip install -r requirements.txt
+```
+
+## Running Code
+
+Create SQLite database with command:
+```
+python manage.py migrate
+```
+
+Run development server with command:
+```
+python manage.py runserver
+```
+
+## Project's Purposes
+
+The code is written for educational purposes as a task in web-development course on the [Devman](https://dvmn.org/).
