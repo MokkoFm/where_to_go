cf55261eb4555206fc155823de9c6707490d12a2
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-22 20:41:46 +0300

    fix null, blank in models, environs instead of dotenv

--- places/models.py
+++ places/models.py
@@ -4,13 +4,12 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name="Title")
-    description_short = models.CharField(
-        max_length=400, null=True, blank=True,
-        verbose_name="Short description")
+    description_short = models.TextField(
+        null=True, blank=True, verbose_name="Short description")
     description_long = HTMLField(
         null=True, blank=True, verbose_name="Long description")
-    lng = models.FloatField(blank=True, verbose_name="Longitude")
-    lat = models.FloatField(blank=True, verbose_name="Latitude")
+    lng = models.FloatField(verbose_name="Longitude")
+    lat = models.FloatField(verbose_name="Latitude")
 
     def __str__(self):
         return f"{self.title}"
@@ -18,12 +17,12 @@ class Place(models.Model):
 
 class Image(models.Model):
     picture = models.ImageField(
-        upload_to="places", null=True, blank=True, verbose_name="Picture")
+        upload_to="places", verbose_name="Picture")
     place = models.ForeignKey(Place, on_delete=models.CASCADE,
-                              related_name="images", default=None, null=True,
-                              blank=True, verbose_name="Place of picture")
+                              related_name="images",
+                              verbose_name="Place of picture")
     position = models.PositiveIntegerField(
-        null=True, blank=True, default=0, verbose_name="Number of picture")
+        blank=True, default=0, verbose_name="Number of picture")
 
     def __str__(self):
         return f"{self.position} {self.place}"
